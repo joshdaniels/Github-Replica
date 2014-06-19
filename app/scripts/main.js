63a23b5ac8e22995641fd26f26504d83e315f99c
Josh Daniels <daniels.joshua100@gmail.com>
2014-06-19 19:17:11 -0400

    finally making some progress,added avatar and name

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,36 +1,13 @@
 var cID = client_id || '';
 var sID = client_secret || '';
-
-
-// $.getJSON('https://api.github.com/users/joshdaniels', {client_id: cID, client_secret: sID}, function(data){
-
-
-// 	console.log(data);
-	
-// 	});
-
 var runtemplate = _.template($(".template").html());
 
-$.getJSON("https://api.github.com/users/joshdaniels/repos", {client_id: cID, client_secret: sID}, function(data){
+$.getJSON('https://api.github.com/users/joshdaniels', {client_id: cID, client_secret: sID}, function(data){
 
-// data.forEach(function(repo){
-// 	$(".name").append(repo.name + "<br />");
+  $(".pi").append(runtemplate(data)); 
+
 	
-// });
-
-_.each(data,function(repo){
-	// $(".name").append(repo.name + "<br />");
-	$(".name").append(runtemplate(repo));
-});
-
 });
 
 
 
-
-
-
-// personal_info.forEach(function(a){
-//   avatar= '<div class="avatar">' + '<img src="' + a.avatar_url + '</div>';
-//   $('.personal_info').append('<div class="avatar">'  + '</div>');
-// });
