2fec446aa68c762c28e773aafa2670548849c8a5
Josh Daniels <daniels.joshua100@gmail.com>
2014-06-19 23:56:13 -0400

    getting there. added right side data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,25 @@
 var cID = client_id || '';
 var sID = client_secret || '';
 var runtemplate = _.template($(".template").html());
+var runtemplate2= _.template($(".template2").html());
+
 
 $.getJSON('https://api.github.com/users/joshdaniels', {client_id: cID, client_secret: sID}, function(data){
 
   $(".pi").append(runtemplate(data)); 
-
-	
+  $(".email_link").add(runtemplate(data));
 });
+
+
+ $.getJSON("https://api.github.com/users/joshdaniels/repos", {client_id: cID, client_secret: sID}, function(data){
+
+_.each(data,function(repo){
+     // $(".right_column").append(repo.name + "<br />");
+     $(".right_column").append(runtemplate2(repo));
+ });
+
+ });
+
 
 
 
