f10d67fcdd26772c10ae18cf2e157f7887d4bf74
Josh Daniels <daniels.joshua100@gmail.com>
2014-06-20 17:19:18 -0400

    added more styling to the right column. fixed append issue with left column

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,12 +2,12 @@ var cID = client_id || '';
 var sID = client_secret || '';
 var runtemplate = _.template($(".template").html());
 var runtemplate2= _.template($(".template2").html());
-
+var runtemplate3= _.template($(".template3").html());
 
 $.getJSON('https://api.github.com/users/joshdaniels', {client_id: cID, client_secret: sID}, function(data){
 
   $(".pi").append(runtemplate(data)); 
-  $(".email_link").add(runtemplate(data));
+  $(".email_link").append(runtemplate3(data));
 });
 
 
